0761941c9892986e049b29abdefa9a98b2448f18
aetrnm <oleggnatkovskii@gmail.com>
2021-04-17 14:45:50 +0300

    first commit

---
+++ convertionHandler.js
@@ -0,0 +1,66 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+const fromCurrencyDropDownList = document.getElementById('fromCurrencyDropDownList');
+const toCurrencyDropDownList = document.getElementById('toCurrencyDropDownList');
+
+const moneyAmount = document.getElementById('amount');
+const resultField = document.getElementById('result');
+
+
+const swapCurrency = document.getElementById('swapButton');
+
+moneyAmount.addEventListener('input', convert);
+fromCurrencyDropDownList.addEventListener('change', convert);
+toCurrencyDropDownList.addEventListener('change', convert);
+
+swapCurrency.addEventListener('click', () => {
+    [fromCurrencyDropDownList.value, toCurrencyDropDownList.value] = [toCurrencyDropDownList.value, fromCurrencyDropDownList.value];
+    console.log('SWAPPED');
+    convert();
+});
+
+async function convert() {
+    var ifConnected = window.navigator.onLine;
+    if (!ifConnected) {
+        alert('No internet connection!');
+        window.close();
+    }
+    const fromCurrency = fromCurrencyDropDownList.value;
+    const toCurrency = toCurrencyDropDownList.value;
+
+    moneyAmount.value = moneyAmount.value.replace(',','.');
+    valueToExchange = moneyAmount.value;
+
+    //Bug fix
+    if(valueToExchange[valueToExchange.length-1] == '.'){
+        valueToExchange = valueToExchange.slice(0, -1);
+    }
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+        .then(res => res.json())
+        .then(res => {
+            const rate = res.rates[toCurrency.toUpperCase()];
+            resultField.value = (valueToExchange * rate).toFixed(2);
+        })
+};
+
+
+//Max 2 nums after dot
+moneyAmount.addEventListener("keyup", function(){
+    moneyAmount.value = moneyAmount.value.replace(/(\.\d{2})\d+/g, '$1');
+});
+
+
+async function CheckValidity(string, e){
+    setTimeout(function(){    
+        if (string.value.split('.').length - 1 > 1){
+            string.value = string.value.slice(0, -1);
+        }
+        if (!(e.key.charCodeAt(0) == 46 || e.key.charCodeAt(0) == 48 || e.key.charCodeAt(0) >= 48 && e.key.charCodeAt(0) <= 57)){
+            string.value = string.value.slice(0, -1);
+        }
+    }, 10);
+}
+
+convert();
